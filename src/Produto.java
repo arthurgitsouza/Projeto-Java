4c5b2667ed295608fa3bcb3a371023d5dec81bd7
Joao Arthur Pereira de Souza <arthurgitsouza@gmail.com>
2025-09-18 12:02:14 -0300

    first commit

---
+++ src/Produto.java
@@ -0,0 +1,11 @@
+public class Produto {
+    String nome;
+    int origem;
+    int categoria;
+    double valorMercadoria;
+    double valorFrete;
+
+
+public Produto(String nome, int origem, int categoria) {}
+
+}
